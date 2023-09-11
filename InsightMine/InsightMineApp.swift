46d9e39251125218376420ab0dcbd63bdcc15dd1
Louis Mille <mille.louis1@gmail.com>
2023-09-11 09:47:59 +0200

    Initial Commit

---
+++ InsightMine/InsightMineApp.swift
@@ -0,0 +1,17 @@
+//
+//  InsightMineApp.swift
+//  InsightMine
+//
+//  Created by Louis Mille on 11/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct InsightMineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
